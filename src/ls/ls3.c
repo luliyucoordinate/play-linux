648a3f53db518cb32d74b2c448c0188aef655e81
coordinate <luliyucoordinate@outlook.com>
2019-04-20 11:34:33 +0800

    add pwd

--- src/ls/ls3.c
+++ src/ls/ls3.c
@@ -21,25 +21,22 @@ void mode_to_letters(int mode, char str[])
     if (S_ISDIR(mode)) str[0] = 'd';
     if (S_ISCHR(mode)) str[0] = 'c';
     if (S_ISBLK(mode)) str[0] = 'b';
-    if (S_ISREG(mode)) str[0] = '-';
-    if (S_IFIFO(mode)) str[0] = 'p';
+    if (S_ISREG(mode)) str[0] = 'r';
+    if (S_IFIFO(mode)) str[0] = 'f';
     if (S_IFLNK(mode)) str[0] = 'l';
     if (S_IFSOCK(mode)) str[0] = 's';
 
     if (mode & S_IRUSR) str[1] = 'r';
     if (mode & S_IWUSR) str[2] = 'w';
     if (mode & S_IXUSR) str[3] = 'x';
-    if (mode & S_ISUID) str[3] = 's';
 
     if (mode & S_IRGRP) str[4] = 'r';
     if (mode & S_IWGRP) str[5] = 'w';
     if (mode & S_IXGRP) str[6] = 'x';
-    if (mode & S_ISGID) str[6] = 's';
 
     if (mode & S_IROTH) str[7] = 'r';
     if (mode & S_IWOTH) str[8] = 'w';
     if (mode & S_IXOTH) str[9] = 'x';
-    if (mode & S_ISVTX) str[9] = 't';
 }
 
 void show_file_info(char *fname, struct stat* buf)
